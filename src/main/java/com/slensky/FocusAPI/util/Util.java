dd05cad332bfa0b59a5ab53df371398506d62853
Stephan Lensky <stephan.lensky@gmail.com>
2016-04-07 11:49:07 -0400

    Add universal date parsing function

--- src/main/java/com/slensky/FocusAPI/util/Util.java
+++ src/main/java/com/slensky/FocusAPI/util/Util.java
@@ -1,7 +1,9 @@
 package com.slensky.FocusAPI.util;
 
+import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -95,6 +97,108 @@ public class Util {
       }
       
       return term;
+   }
+   
+   public static Calendar parseDate(String date) {
+      
+      int month, day, year;
+      int hour = -1, minute = -1;
+      
+      date = date.toLowerCase();
+      String[] dayPrefixes = {"mon", "tue", "wed", "thu", "fri", "sat", "sun"};
+      String[] monthPrefixes = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
+      
+      /* get rid of the day label at the front if there is one */
+      
+      String potentialDay = date.substring(0, 3);
+      boolean isDay = false;
+      for (String d : dayPrefixes) {
+         if (d.equals(potentialDay)) {
+            isDay = true;
+            break;
+         }
+      }
+      
+      if (isDay) {
+         date = date.substring(4);
+      }
+      
+      /* get rid of a leading zero for the month if there is one */
+      
+      if (date.charAt(0) == '0') {
+         date = date.substring(1);
+      }
+      
+      /* convert word month to numeric month */
+      
+      int numericFirstChar = Character.getNumericValue(date.charAt(0));
+      if (!(numericFirstChar > 0 && numericFirstChar < 10)) {
+         
+         String prefix = date.substring(0, 3);
+         
+         int monthValue = 0;
+         for (int i = 0; i < monthPrefixes.length; i++) {
+            if (monthPrefixes[i].equals(prefix)) {
+               monthValue = i + 1;
+               break;
+            }
+         }
+         assert monthValue != 0 : "Month could not be identified";
+         
+         month = monthValue;
+         
+      }
+      else {
+         month = Integer.parseInt(date.substring(0, date.indexOf(' ')));
+      }
+      
+      date = date.substring(date.indexOf(' ') + 1);
+      
+      /* get day */
+      
+      String dayStr = date.substring(0, 2);
+      if (dayStr.startsWith("0")) {dayStr = dayStr.substring(1);}
+      if (dayStr.endsWith("t") || dayStr.endsWith(",")) {dayStr = dayStr.substring(0, 1);}
+      day = Integer.parseInt(dayStr);
+      
+      /* get year */
+      
+      date = date.substring(date.indexOf(' ') + 1);
+      if (date.contains(" ")) {
+         year = Integer.parseInt(date.substring(0, date.indexOf(' ')));
+      }
+      else {
+         year = Integer.parseInt(date);
+      }
+      
+      /* get time if there is one */
+      
+      if (date.contains(" ")) {
+         date = date.substring(date.indexOf(' ') + 1);
+         
+         String[] hoursMinutes = date.substring(0, date.indexOf(' ')).split(":");
+         assert hoursMinutes.length == 2 : "Error splitting time field";
+         
+         hour = Integer.parseInt(hoursMinutes[0]);
+         minute = Integer.parseInt(hoursMinutes[1]);
+         
+         if (date.contains("pm")) {
+            hour += 12;
+         }
+         if (hour == 12 || hour == 24) {
+            hour -= 12;
+         }
+         
+      }
+      
+      Calendar parsed = Calendar.getInstance();
+      parsed.set(year, month - 1, day);
+      if (hour != -1 && minute != -1) {
+         parsed.set(Calendar.HOUR, hour);
+         parsed.set(Calendar.MINUTE, minute);
+      }
+      
+      return parsed;
    }
    
 }
